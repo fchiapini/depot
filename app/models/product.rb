019c3c5f42f4ab5ccc0d7089ec3b65880f329f2c
Felipe <fchiapini@gmail.com>
2015-12-22 15:27:58 -0200

    Caching

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,8 @@ class Product < ActiveRecord::Base
 	validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i, 
 	message: 'must be a URL for GIF, JPG or PNG image.' }
 	validates :title, length: { minimum: 10, message: 'the title must have at least 10 characters' }
+
+	def self.latest
+		Product.order(:updated_at).last
+	end
 end
